b7d034bb9c817d2400cfb247e141d99d5b2c7cfb
karudedios <c.dedios@outlook.com>
2014-10-10 15:57:09 -0400

    Extension for HTTP Status Codes

---
+++ NancyTest/Customs/NancyHttpStatusCodeExtension.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace NancyTest.Customs
+{
+    public static class NancyHttpStatusCodeExtension
+    {
+        public static bool IsError(this HttpStatusCode self)
+        {
+            return ((int) self) > 400 && ((int) self) < 599;
+        }
+    }
+}
