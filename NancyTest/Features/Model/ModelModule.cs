fe438b4069bd1b2b85fb806f5b62d7b0cbac0c7e
karudedios <c.dedios@outlook.com>
2014-10-13 17:58:46 -0400

    Possibility adding new model item

---
+++ NancyTest/Features/Model/ModelModule.cs
@@ -0,0 +1,23 @@
+﻿using Nancy;
+using Nancy.ModelBinding;
+using NancyTest.DAL.Extensions;
+using NancyTest.DAL.Implementation.Abstract;
+
+namespace NancyTest.Features.Model
+{
+    public class ModelModule : NancyModule
+    {
+        public ModelModule(IProvider provider)
+        {
+            Get["/Add"] = parameters =>
+                Negotiate
+                    .WithView("Add");
+
+            Put["/Add"]
+                = parameters =>
+                    Negotiate
+                        .WithModel(provider.Add(this.Bind<DAL.Models.Model>()))
+                        .AsJSON();
+        }
+    }
+}
