ebaf8d2e79b55e7e2a46367081494ecc6a2575cb
karudedios <c.dedios@outlook.com>
2014-10-13 17:59:52 -0400

    Configurable codes from settings

--- NancyTest/Bootstrapper.cs
+++ NancyTest/Bootstrapper.cs
@@ -14,45 +14,22 @@ namespace NancyTest
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
-        // The bootstrapper enables you to reconfigure the composition of the framework,
-        // by overriding the various methods and properties.
-        // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
-
         protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
         {
-            CustomStatusCode.AddCode(400);
-            CustomStatusCode.AddCode(401);
-            CustomStatusCode.AddCode(404);
-            CustomStatusCode.AddCode(500);
+            CustomStatusCode.AddCode (
+                    Properties.Settings.Default.ErrorCodes
+                    .Split(',')
+                    .Select(int.Parse)
+                );
 
             CustomStatusCode.AllowIISErrors = true;
-
             base.ApplicationStartup(container, pipelines);
             container.Register<IProvider>(new Provider());
 
             Conventions.ViewLocationConventions.Clear();
             Conventions.ViewLocationConventions.Add((viewName, model, context) => string.Concat("Layout/", viewName));
-            Conventions.ViewLocationConventions.Add((viewName, model, context) => string.Concat("Codes/", viewName));
-            Conventions.ViewLocationConventions.Add((viewName, model, context) => string.Concat(context.ModuleName, "/", viewName));
-
-            //pipelines.OnError += (ctx, err) => HandleExceptions(err, ctx); ;
+            Conventions.ViewLocationConventions.Add((viewName, model, context) => string.Concat("Features/Errors/", viewName));
+            Conventions.ViewLocationConventions.Add((viewName, model, context) => string.Concat("Features/" + context.ModuleName, "/", viewName));
         }
-
-        /*private static Response HandleExceptions(Exception ex, NancyContext ctx)
-        {
-            var result = new Response
-            {
-                  ReasonPhrase = ex.Message
-                , StatusCode = (ex is NotImplementedException)
-                    ? HttpStatusCode.NotImplemented
-                    : (ex is UnauthorizedAccessException)
-                        ? HttpStatusCode.Unauthorized
-                        : (ex is ArgumentException || ex is NotProperRequestException)
-                            ? HttpStatusCode.BadRequest
-                            : HttpStatusCode.InternalServerError
-            };
-
-            return result;
-        }*/
     }
 }
